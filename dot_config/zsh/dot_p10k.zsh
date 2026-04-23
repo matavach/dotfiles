e9ad8558559858da715531f5e1374b14e846e568
Erik Matijevich <erik@matijevich.org>
2026-04-23 08:37:32 -0500

    Update .config/zsh/.p10k.zsh

--- dot_config/zsh/dot_p10k.zsh
+++ dot_config/zsh/dot_p10k.zsh
@@ -453,13 +453,13 @@
     #
     (( VCS_STATUS_HAS_UNSTAGED == -1 )) && res+=" ${modified}─"
     if (( VCS_STATUS_COMMITS_AHEAD || VCS_STATUS_COMMITS_BEHIND )); then
-      res+=" ${bracket}["
+      res+=" ${bracket}[ "
       # ⇣42 if behind the remote.
-      (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${clean}󰁅 ${VCS_STATUS_COMMITS_BEHIND}"
+      (( VCS_STATUS_COMMITS_BEHIND )) && res+="${clean}󰁅${VCS_STATUS_COMMITS_BEHIND}"
       # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
       (( !VCS_STATUS_COMMITS_AHEAD && VCS_STATUS_COMMITS_BEHIND )) && res+=" "
-      (( VCS_STATUS_COMMITS_AHEAD  )) && res+=" ${clean}󰁝 ${VCS_STATUS_COMMITS_AHEAD}"
-      res+="${bracket}]"
+      (( VCS_STATUS_COMMITS_AHEAD  )) && res+="${clean}󰁝${VCS_STATUS_COMMITS_AHEAD}"
+      res+="${bracket} ]"
     elif [[ -n $VCS_STATUS_REMOTE_BRANCH ]]; then
       # Tip: Uncomment the next line to display '=' if up to date with the remote.
      res+="${clean} "
