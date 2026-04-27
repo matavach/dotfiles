4e9a72038af8a1ddc14dc23fd793e6f4346514f9
Erik Matijevich <erik@matijevich.org>
2026-04-27 12:56:30 -0500

    Update .config/zsh/.p10k.zsh

--- dot_config/zsh/dot_p10k.zsh
+++ dot_config/zsh/dot_p10k.zsh
@@ -453,9 +453,9 @@
     #
     (( VCS_STATUS_HAS_UNSTAGED == -1 )) && res+=" ${modified}─"
     if (( VCS_STATUS_COMMITS_AHEAD || VCS_STATUS_COMMITS_BEHIND )); then
-      res+=" ${bracket}["
+      res+=" ${bracket}[ "
       # ⇣42 if behind the remote.
-      (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${clean}󰁅 ${VCS_STATUS_COMMITS_BEHIND}"
+      (( VCS_STATUS_COMMITS_BEHIND )) && res+="${clean}󰁅 ${VCS_STATUS_COMMITS_BEHIND}"
       # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
       (( !VCS_STATUS_COMMITS_AHEAD && VCS_STATUS_COMMITS_BEHIND )) && res+=" "
       (( VCS_STATUS_COMMITS_AHEAD  )) && res+="${clean}󰁝 ${VCS_STATUS_COMMITS_AHEAD} "
